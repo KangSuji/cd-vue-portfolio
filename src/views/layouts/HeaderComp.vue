d0629916a8bc283b8e8038ea38fe60513237ccba
KangSuji <sukang0661@gmail.com>
2025-04-13 02:23:56 +0900

    feat: 스타일 시트 추가 및 구조 변경

---
+++ src/views/layouts/HeaderComp.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="sticky_header">
+    <h1>LOGO</h1>
+    <ul>
+      <li>home</li>
+      <li>list</li>
+    </ul>
+  </header>
+</template>
+<script lang="ts" setup></script>
---
+++ src/views/pages/MainView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page_container">
+    <HeaderComp />
+    <main class="main_container">main</main>
+  </div>
+</template>
+<script lang="ts" setup>
+import HeaderComp from '../layouts/HeaderComp.vue'
+</script>
